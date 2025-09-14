478c20d33bce5376e6dc78a44e94d82e8c9dad24
Vivek Yadav <vy6751536@gmail.com>
2025-09-14 15:34:26 +0530

    created the signup and sigin endpoint for admin

--- routers/admin.js
+++ routers/admin.js
@@ -1,15 +1,49 @@
 const {Router}=require('express')
 const adminRouter=Router()
 const {adminModel, userMOdel}=require('../db')
+const jwt=require('jsonwebtoken')
+const JWT_ADMIN_PASSWORD='asdas123'
 //bcrypt ,zod 
-adminRouter.post('/signup', function(req,res){
-
+adminRouter.post('/signup', async function(req,res){
+const {email,password,firstName,lastName}=req.body;
+    //todo:adding zod for validtion
+    //todo hash the password so plaintext pw is not stored in the db
+    // todo :put inside a try catch block
+     await adminModel.create({
+        email:email,
+        password:password,
+        firstName:firstName,
+        lastName:lastName
+    })
 res.json({
     message:'you are signup succesfully'
 })
 })
-adminRouter.post('/signin',function(req,res){
-    const password=req.body.password
+adminRouter.post('/signin',async function(req,res){
+    
+      const {email,password}=req.body;
+    const admin=await adminModel.findOne({
+        email:email,
+        password:password
+    });
+    if(admin){
+   const token=jwt.sign({
+    id:admin._id
+   },JWT_ADMIN_PASSWORD)
+
+   res.json({
+    token:token
+})
+
+    }
+
+else{
+
+res.status(403).json({
+msg:'incorrect credential'
+})
+}
+
 res.json({
     message:'this is signin endpoint'
 })
